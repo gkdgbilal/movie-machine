a1e086cb971d56af90d721485045033659ac303b
Bilal GOKDAG <38653100+gkdgbilal@users.noreply.github.com>
2022-04-27 23:05:03 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
       <div className="font-bold text-neutral-100 p-4 max-w-7xl mx-auto container tracking-widest font-neue">
         <Link href="/">
           <a className="text-base md:text-2xl">
-            Movie<span className="text-[#F96E26]">Machine</span>
+            MOVIE<span className="text-[#F96E26]">MACHINE</span>
           </a>
         </Link>
       </div>
